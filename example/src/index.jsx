a23ce396e16a385b5e365c610045e240d699cbb6
lindakai2016 <398017990@qq.com>
2018-06-25 13:54:19 +0800

    宽度未占满

--- example/src/index.jsx
+++ example/src/index.jsx
@@ -7,7 +7,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <ReactTrend width={100} height={50} stroke="red" strokeWidth={1}/>
+                <ReactTrend width={100} height={50} stroke="red" strokeWidth={1} trends={[1]}/>
                 <ReactTrend width={200} height={50} stroke="green" strokeWidth={2}/>
                 <ReactTrend width={300} height={50} stroke="#ccc" strokeWidth={3}/>
                 <ReactTrend width={200} height={100} stroke="#00ff99" strokeWidth={2}/>
--- src/index.jsx
+++ src/index.jsx
@@ -18,12 +18,15 @@ class ReactTrend extends React.Component {
         let width=this.state.width;
         let height=this.state.height;
         let trends=this.state.trends;
+        if(trends.length<2) return (
+            <span>trends长度不小于2</span>
+        );
 
         let ratio = this.state.height/this.state.width;
         let viewHeight=100 * ratio;
         let viewBox = "0 0 100 " + viewHeight;
 
-        let dx = Math.floor(100 / trends.length);
+        let dx = Math.floor(100 / (trends.length - 1));
         let max = trends[0];
         let min = trends[0];
         for(let i=0;i<trends.length;i++){
